0785a996ea439cd27e8d99b843b4a1d3cbac152b
Nikolaj Majorov <nikolaj@majorov.biz>
2022-06-16 02:25:56 +0200

    change deployment script

--- quarkus-backend/deploy_openshift4.sh
+++ quarkus-backend/deploy_openshift4.sh
@@ -4,7 +4,8 @@ echo "deploy app"
 echo
 oc new-app  https://github.com/visually-impered-hackaton-2022/demo-persons.git  \
  --context-dir=quarkus-backend \
--e POSTGRESQL_USER=niko \
--e POSTGRESQL_PASSWORD=niko \
--e POSTGRESQL_DATABASE=niko \
+-e POSTGRESQL_USER=postgres \
+-e POSTGRESQL_PASSWORD=Kv2oru7HZv \
+-e POSTGRESQL_DATABASE=postgres \
+-e POSTGRESQL_SERVICE_NAME=nmpostgresql \
     --image-stream=java:openjdk-11-el7
